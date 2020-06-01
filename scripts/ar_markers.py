0bba329f6ba13ae5527147564f8946ac16615978
paclo <pcandelas98@gmail.com>
2020-06-01 14:57:10 -0500

    code properly commented

--- scripts/ar_markers.py
+++ scripts/ar_markers.py
@@ -1,11 +1,20 @@
+"""This program generates the marker that will be detected and followed
+in future programs.
+And generates a png document:"marker33.png" in the same directory.
+Author:Pablo Candelas 01/jun/2020"""
+# import libraries
 import cv2 as cv
 import numpy as np
 
 # Load the predefined dictionary
 dictionary = cv.aruco.Dictionary_get(cv.aruco.DICT_6X6_250)
 
-# Generate the marker
+# Generate the 33rth marker from the aruco library
 markerImage = np.zeros((200, 200), dtype=np.uint8)
 markerImage = cv.aruco.drawMarker(dictionary, 33, 200, markerImage, 1);
 
+# Generate de document
 cv.imwrite("marker33.png", markerImage);
+
+# Farewell msg
+print("\n\n\tBYEEE\n\n")
