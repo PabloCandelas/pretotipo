484d1b5d237074da9867cb8840b50f92de6d8a7f
paclo <pcandelas98@gmail.com>
2020-06-01 16:54:56 -0500

    codigo comentado

--- scripts/ar_tracker.py
+++ scripts/ar_tracker.py
@@ -1,68 +1,94 @@
+"""
+This program detects the markers from aruco library.
+The input is:
+    *The video from the webcam (you must have your webcam enabled)
+The outputs are:
+    *The center of the marker in the video (pixels)
+    *The radious of the circumscribed circle around the marker (pixels) 
+    
+Author:Pablo Candelas 01/june/2020
+"""
 #!/usr/bin/env python
-#librerias
+
+# libraries
 import cv2
 import numpy as np
 
-#constantes
+# Constants
 DEBUG = True 
-EJEX = 0
-EJEY=1
+AXISX = 0
+AXISY=1
 
-def esquinas(markerCorners,esq,eje):
+# Functions
+def esquinas(markerCorners,coners,axis):
+"""This function receives the array of arrays "markerCorners" and the "axis" and it returns the array "corners" 
+only with the values from the desired axis from "markerCorners". """
     for i in range(0,4):
-        esq[i] = markerCorners[0][0][i][eje]
-#main
+        esq[i] = markerCorners[0][0][i][axis]
+
+#Main program
 if __name__ == '__main__':
-    #empezar a tomar video
+    # Recive the incoming video
     print('Press "q" to quit')
     capture = cv2.VideoCapture(0)
     if capture.isOpened():  # try to get the first frame
         frame_captured, frame = capture.read()
     else:
         frame_captured = False
-    #mientras se reciba video
+        
+    # Meanwhile the video is active
     while frame_captured:
-        #DETECTAR MARCADOR
-        #Load the dictionary that was used to generate the markers.
+    
+        # DETECT THE MARKER
+        # Load the dictionary that was used to generate the markers.
         dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
         # Initialize the detector parameters using default values
         parameters =  cv2.aruco.DetectorParameters_create()
         # Detect the markers in the image
-        markerCorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(frame, dictionary, parameters=parameters)     
-        #Si se detecto un marcador
+        markerCorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(frame, dictionary, parameters=parameters)   
+          
+        # If there was at least one marker detected
         if markerIds >0:
-            #dibujar cuadro alrededor
-            cv2.aruco.drawDetectedMarkers(frame, markerCorners, markerIds)        
-            #calculo de centro y radio
-            esqx = [0,0,0,0]
-            esquinas(markerCorners,esqx,EJEX)
-            esqy = [0,0,0,0]
-            esquinas(markerCorners,esqy,EJEY) 
+            # Draw a rectangle around the detected marker
+            cv2.aruco.drawDetectedMarkers(frame, markerCorners, markerIds)  
+                  
+            # Computing the center and radious
+            xcorners = [0,0,0,0]
+            esquinas(markerCorners,xcorners,AXISX)
+            ycorners = [0,0,0,0]
+            esquinas(markerCorners,ycorners,AXISY) 
+            """ To calculate the center we know that the marker has a quafrangular shape
+            # x coordinates will be the avarage of the minimun value and the maximun value of the corners in the "x" axis
+            # y coordinates will be the avarage of the minimun value and the maximun value of the corners in the "y" axis """
             center =int((min(esqx) + max(esqx))/2) ,int((min(esqy) + max(esqy))/2)
+            # The radious is computed used the distance from the center of the marker to any of its corners
             radius = np.sqrt((esqx[0]-center[0])*(esqx[0]-center[0]) + (esqy[0]-center[1])*(esqy[0]-center[1]))
+            # Print the values of the variables "center" and "radious" 
             if DEBUG: 
-                #print(esqx)
-                #print(esqy)
                 print("----------")
                 print("center:")
                 print(center) 
                 print("radius:")  
                 print(radius)  
-             #Dibujar circulo y centro
+                
+            # Draw the circle thar circumscribes the marker
             cv2.circle(frame, (int(center[0]), int(center[1])), int(radius),
             (0, 128, 255), 2)
+            # Draw the center with a small circle
             cv2.circle(frame, center, 5, (255, 255, 0), -1)     
-            #mostrar en pantalla     
+            
+        # Show the resulting image/video 
         cv2.imshow('Test Frame', frame)
-        #exit
+        
+        # Exit
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-        #retomar capture
+        # Retake the video capture
         frame_captured, frame = capture.read()
 
     # When everything done, release the capture
     capture.release()
-    cv2.destroyAllWindows()
-    print("")
-    print("  BYEEE   ")
-    print("")
+    cv2.destroyAllWindows() 
+    
+    # Farewell msg
+    print("\n\n\tBYEEE\n\n")
