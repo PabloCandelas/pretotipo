75069d643f57b6453bf5609c692579e158d4e175
paclo <pcandelas98@gmail.com>
2020-06-02 10:10:12 -0500

    outnode.py

--- scripts/ar_tracker_n.py
+++ scripts/ar_tracker_n.py
@@ -87,7 +87,7 @@ class ArTracker():
         
         if self.blah == 1:
             # Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
-            self.out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (640,480))
+            self.out = cv2.VideoWriter('outnode.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (640,480))
             self.blah = 2
             
         # DETECT THE MARKER
